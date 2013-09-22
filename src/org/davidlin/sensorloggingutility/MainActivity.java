1731567de01c6506b1521261ee27e3880bd3fb9d
David Lin <linwdav@gmail.com>
2013-09-22 02:39:27 -0700

    Added fixed tabs and fragments

--- src/org/davidlin/sensorloggingutility/MainActivity.java
+++ src/org/davidlin/sensorloggingutility/MainActivity.java
@@ -1,16 +1,20 @@
 package org.davidlin.sensorloggingutility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
-import android.app.Activity;
-import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-	
+public class MainActivity extends FragmentActivity {
+
 	public final String LOGTAG = "org.davidlin";
+	List<Fragment> tabFragments;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +31,46 @@ public class MainActivity extends Activity {
 	}
 
 	private void setupActionBar() {
+		// Add tab fragments into list
+		tabFragments = new ArrayList<Fragment>();
+		tabFragments.add(new MainFragment());
+		tabFragments.add(new SetFreqFragment());
+		tabFragments.add(new ScriptFragment());
+		
 		// Obtain action bar
-		final ActionBar aBar = getActionBar();
+		final ActionBar actionBar = getActionBar();
 		// Set action bar to tab mode
-		aBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		// Add tab listener to handle changing of current fragment
 		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
 
 			@Override
-			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			public void onTabReselected(Tab tab, android.app.FragmentTransaction ft) {
 				Log.d(LOGTAG, "onTabReselected");
 			}
 
 			@Override
-			public void onTabSelected(Tab tab, FragmentTransaction ft) {
-				// mViewPager.setCurrentItem(tab.getPosition());
+			public void onTabSelected(Tab tab, android.app.FragmentTransaction ft) {
 				Log.d(LOGTAG, "onTabSelected");
+				Log.d(LOGTAG, "The tab position is " + tab.getPosition());
+				Fragment fragment = tabFragments.get(tab.getPosition());
+				Bundle args = new Bundle();
+				args.putInt("", tab.getPosition());
+				fragment.setArguments(args);
+				//getSupportFragmentManager().beginTransaction().replace(R.id.fragment1, fragment).commit();
+				getSupportFragmentManager().beginTransaction().add(R.id.fragment1, fragment).commit();
 			}
 
 			@Override
-			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			public void onTabUnselected(Tab tab, android.app.FragmentTransaction ft) {
 				Log.d(LOGTAG, "onTabUnselected");
+				getSupportFragmentManager().beginTransaction().remove(getSupportFragmentManager().findFragmentById(R.id.fragment1)).commit();
 			}
-
 		};
 		// Add tabs and register event handlers
-		aBar.addTab(aBar.newTab().setText("MAIN").setTabListener(tabListener));
-		aBar.addTab(aBar.newTab().setText("SET FREQ").setTabListener(tabListener));
-		aBar.addTab(aBar.newTab().setText("SCRIPT").setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("MAIN").setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("SET FREQ").setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("SCRIPT").setTabListener(tabListener));
 	}
 
 }
