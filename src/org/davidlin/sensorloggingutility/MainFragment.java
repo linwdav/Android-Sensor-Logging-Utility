6cf16c52a2b9c2c6595906bcf23ba32a1b6f9b55
David Lin <linwdav@gmail.com>
2013-09-22 18:44:19 -0700

    Added layout to Main tab

--- src/org/davidlin/sensorloggingutility/MainFragment.java
+++ src/org/davidlin/sensorloggingutility/MainFragment.java
@@ -5,17 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainFragment extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate and return the layout
-		// return inflater.inflate(R.layout.setfreq_fragment, container, false);
-		TextView textView = new TextView(getActivity());
-		textView.setText("MAIN FRAGMENT");
-		return textView;
+		return inflater.inflate(R.layout.fragment_main, container, false);
 	}
 	
 }
